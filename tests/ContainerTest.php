e1bbb95047f0f2d401f9737ac51abdd49b23b25c
Zlatin Hristov <zlatin.hristov@devop.studio>
2019-05-15 14:17:03 +0300

    Remove PHP5 support

--- tests/ContainerTest.php
+++ tests/ContainerTest.php
@@ -2,35 +2,36 @@
 namespace DevOp\Core\Test;
 
 use DevOp\Core\Container;
+use PHPUnit\Framework\TestCase;
 
-class ContainerTest extends \PHPUnit_Framework_TestCase
+class ContainerTest extends TestCase
 {
 
     public function testSetAndGetContainer()
     {
         $container = new Container();
-        
+
         $container->add('int', 3);
-        
+
         $this->assertTrue($container->has('int'));
         $this->assertEquals(3, $container->get('int'));
     }
-    
+
     public function testThrowException()
     {
-        $this->setExpectedException('\DevOp\Core\Exception\NotFoundException');
+        $this->expectException('\DevOp\Core\Exception\NotFoundException');
         $container = new Container();
         $container->get('missing');
     }
-    
+
     public function testSetMultipleService()
     {
         $container = new Container();
         $container->add('string', 'test string');
-        $this->setExpectedException('\DevOp\Core\Exception\ContainerException');
+        $this->expectException('\DevOp\Core\Exception\ContainerException');
         $container->add('string', 'new string');
     }
-    
+
     public function testSharedObjects()
     {
         $class = new \stdClass();
